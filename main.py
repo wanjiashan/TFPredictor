ce6e087daa632b359ed438bff5f307d506001361
Jiashan Wan <32283464+wanjiashan@users.noreply.github.com>
2024-12-02 12:47:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,28 @@ import argparse
 import numpy as np
 import pandas as pd
 from prepare import PrepareDataset
-from train_STGmamba import TrainSTG_Mamba, TestSTG_Mamba
+from train_TFPredictor import TrainTFPredictor, TestTFPredictor
 from train_rnn import TrainLSTM, TestLSTM
+import matplotlib.pyplot as plt
 
 
-parser = argparse.ArgumentParser(description='Train & Test STG_Mamba for traffic/weather/flow forecasting')
+
+parser = argparse.ArgumentParser(description='Train & Test TFPredictor for traffic/weather/flow forecasting')
 # choose dataset
-parser.add_argument('-dataset', type=str, default='pems04', help='which dataset to run [options: know_air, pems04, hz_metro]')
+parser.add_argument('-dataset', type=str, default='PESMBAY', help='which dataset to run [options: PEMSBY, pems04, metr-la]')
 # choose model
-parser.add_argument('-model', type=str, default='STGmamba', help='which model to train & test [options: STGmamba, lstm]')
+parser.add_argument('-model', type=str, default='TFPredictor', help='which model to train & test [options: TFPredictor.py]')
 # choose number of node features
-parser.add_argument('-mamba_features', type=int, default=307, help='number of features for the STGmamba model [options: 307,184,80]')
+parser.add_argument('-mamba_features', type=int, default=325, help='number of features for the STGmamba model [options: 325,307,207]')
 
 args = parser.parse_args()
 
 ###### loading data #######
 
-if args.dataset == 'know_air':
-    print("\nLoading KnowAir Dataset...")
-    speed_matrix = pd.read_csv('Know_Air/knowair_temperature.csv', sep=',')
-    A = np.load('Know_Air/knowair_adj_mat.npy')
+if args.dataset == 'PEMSBY':
+    print("\nLoading pems08 data...")
+    speed_matrix = pd.read_csv('PEMSBY/pemsby_flow.csv', sep=',')
+    A = np.load('PEMSBY/pemsby-dtw-288-1-.npy')
 
 elif args.dataset == 'pems04':
     print("\nLoading PEMS04 data...")
@@ -41,10 +43,7 @@ elif args.dataset == 'PEMS07':
     print("\nLoading HZ-Metro data...")
     speed_matrix = pd.read_csv('PEMS07/pems07_flow.csv', sep=',')
     A = np.load('PEMS07/PEMS07-dtw-288-1-.npy')
-elif args.dataset == 'PEMSBY':
-    print("\nLoading PEMSBY data...")
-    speed_matrix = pd.read_csv('PEMSBY/pemsby_flow.csv', sep=',')
-    A = np.load('PEMSBY/pemsby-dtw-288-1-.npy')
+
 elif args.dataset == 'metr-la':
     print("\nLoading metr-la data...")
     speed_matrix = pd.read_csv('metr-la/metr-la_flow.csv', sep=',')
@@ -55,16 +54,8 @@ print(f"Train dataloader length: {len(train_dataloader)}")
 print(f"Validation dataloader length: {len(valid_dataloader)}")
 print(f"Test dataloader length: {len(test_dataloader)}")
 
-# models you want to use
-# models you want to use
-if args.model == 'STGmamba':
+if args.model == 'TFPredictor':
     print("\nTraining STGmamba model...")
-    STGmamba, STGmamba_loss = TrainSTG_Mamba(train_dataloader, valid_dataloader, A, K=3, num_epochs=200, mamba_features=args.mamba_features)
+    TFPredictor, TFPredictor_loss = TrainTFPredictor(train_dataloader, valid_dataloader, A, K=3, num_epochs=200, mamba_features=args.mamba_features)
     print("\nTesting STGmamba model...")
-    results = TestSTG_Mamba(STGmamba, test_dataloader, max_value)
-
-elif args.model == 'lstm':
-    print("\nTraining lstm model...")
-    lstm, lstm_loss = TrainLSTM(train_dataloader, valid_dataloader, num_epochs=200)
-    print("\nTesting lstm model...")
-    results = TestLSTM(lstm, test_dataloader, max_value)
+    results = TestTFPredictor(TFPredictor, test_dataloader, max_value)
